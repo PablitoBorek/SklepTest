149089207209aac3a8de1d6aa368914c13fa8722
Paweł Borkowski <pawel.borkowski@hycom.pl>
2023-10-06 02:00:53 +0200

    Webdriver Setup

---
+++ src/main/java/DriverManager.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverManager {
+
+    public static WebDriver driver;
+
+    public static void startDriver(){
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    public static void stopDriver(){
+        driver.quit();
+    }
+}
